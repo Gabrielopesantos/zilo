4a9df82aac977027bf66b28ab2354a417101d9b9
Gabriel Santos <gabrielopesantos97@gmail.com>
2024-09-22 22:04:54 +0100

    chapt3-windowsize-wip
    Signed-off-by: Gabriel Santos <gabrielopesantos97@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -3,22 +3,33 @@ const std = @import("std");
 const io = std.io;
 const linux = std.os.linux;
 const ascii = std.ascii;
+const mem = std.mem;
 
 const STDIN = io.getStdIn().reader();
 const STDOUT = io.getStdOut().writer();
 const STDIN_FILENO = linux.STDIN_FILENO;
+const STDOUT_FILENO = linux.STDOUT_FILENO;
+
+const Key = enum(u8) { ctrl_q = 17, _ };
 
 const Editor = struct {
     const Self = @This();
 
+    allocator: mem.Allocator,
+    c: [1]u8 = undefined,
     orig_termios: linux.termios = undefined,
+    num_rows: u16 = 0,
+    num_cols: u16 = 0,
 
-    fn init() !Self {
-        return Self{};
+    fn init(allocator: mem.Allocator) !Self {
+        return Self{
+            .allocator = allocator,
+            .c = [_]u8{0},
+        };
     }
 
     fn deinit(self: *Self) void {
-        _ = linux.tcsetattr(STDIN_FILENO, .FLUSH, &self.orig_termios);
+        self.disableRawMode();
     }
 
     fn enableRawMode(self: *Self) !void {
@@ -74,23 +85,81 @@ const Editor = struct {
 
         _ = linux.tcsetattr(STDIN_FILENO, .FLUSH, &termios);
     }
+
+    fn disableRawMode(self: *Self) void {
+        _ = linux.tcsetattr(STDIN_FILENO, .FLUSH, &self.orig_termios);
+    }
+
+    fn readKey(self: *Self) !void {
+        while (true) {
+            // NOTE: VTIME is not being respected, `read` is hanging;
+            _ = linux.read(STDIN_FILENO, &self.c, 1);
+            break;
+        }
+    }
+
+    fn processKeyPress(self: *Self) !void {
+        try self.readKey();
+        switch (@as(Key, @enumFromInt(self.c[0]))) {
+            .ctrl_q => {
+                self.disableRawMode();
+                linux.exit(0);
+            },
+            else => {},
+        }
+    }
+
+    fn refreshScreen(self: *Self) !void {
+        // Writting 4 bytes out to the terminal, `\x1b` (27) and `[2J`.
+        // Escape sequences always start with `Escape` followed by `[`
+        // We are using the J command (Erase In Display) to clear the screen.
+        // Escape sequence commands take arguments, which come before the command.
+        // In this case the argument is 2, which says to clear the entire screen
+        _ = try STDOUT.writeAll("\x1b[2J");
+        // H command, which is only 3 bytes long, puts the cursor at a certain position (1, 1) by default;
+        _ = linux.write(linux.STDOUT_FILENO, "\x1b[H", 3);
+
+        try self.drawRows();
+
+        _ = linux.write(linux.STDOUT_FILENO, "\x1b[H", 3);
+    }
+
+    fn drawRows(self: *Self) !void {
+        for (0..self.num_cols) |_| {
+            _ = try STDOUT.write("~\r\n");
+        }
+    }
+
+    fn getWindowSize(self: *Self) !void {
+        var winsize: std.posix.winsize = undefined;
+        if (linux.ioctl(
+            STDOUT_FILENO,
+            0x5413, // TIOCGWINSZ
+            @intFromPtr(&winsize),
+        ) == -1 or winsize.col == 0) {
+            const strn = "\x1b[999C\x1b[999B";
+            _ = linux.write(STDOUT_FILENO, strn, strn.len);
+        } else {
+            self.num_rows = winsize.row;
+            self.num_cols = winsize.col;
+        }
+    }
 };
 
 pub fn main() !void {
-    var editor = try Editor.init();
+    var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa_impl.allocator();
+    defer _ = gpa_impl.deinit();
+
+    var editor = try Editor.init(allocator);
     defer editor.deinit();
 
-    try editor.enableRawMode();
+    // NOTE: Temporary
+    try editor.getWindowSize();
 
-    var c = [_]u8{0};
+    try editor.enableRawMode();
     while (true) {
-        // NOTE: VTIME is not being respected, `read` is hanging;
-        _ = linux.read(STDIN_FILENO, &c, 1);
-        if (ascii.isControl(c[0])) {
-            try STDOUT.print("{}\r\n", .{c[0]});
-        } else {
-            try STDOUT.print("{} {c}\r\n", .{ c[0], c[0] });
-        }
-        if (c[0] == 'q') break;
+        try editor.refreshScreen();
+        try editor.processKeyPress();
     }
 }
